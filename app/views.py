48466d3a19e58d8eac937fc8b09e7251859b086d
EidAbdullahi <eidabdullahi10@gmail.com>
2020-12-01 12:10:07 +0300

    commit changes

--- app/views.py
+++ app/views.py
@@ -1,4 +1,4 @@
-rom flask import render_template
+from flask import render_template
 from app import app
 from .request import get_movies,get_movie
 
@@ -31,3 +31,19 @@ def index():
     now_showing_movie = get_movies('now_playing')
     title = 'Home - Welcome to The best Movie Review Website Online'
     return render_template('index.html', title = title, popular = popular_movies, upcoming = upcoming_movie, now_showing = now_showing_movie )
+
+
+    def search_movie(movie_name):
+        search_movie_url = 'https://api.themoviedb.org/3/search/movie?api_key={}&query={}'.format(api_key,movie_name)
+    with urllib.request.urlopen(search_movie_url) as url:
+        search_movie_data = url.read()
+        search_movie_response = json.loads(search_movie_data)
+
+        search_movie_results = None
+
+        if search_movie_response['results']:
+            search_movie_list = search_movie_response['results']
+            search_movie_results = process_results(search_movie_list)
+
+
+    return search_movie_results
--- instance/config.py
+++ instance/config.py
@@ -1,1 +1,2 @@
 MOVIE_API_KEY = 'dc8778f07dfcbb76925d7106d12b5dfb'
+SECRET_KEY = 'Flask WTF Secret Key'
