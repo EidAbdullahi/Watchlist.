aaeacd099058ead9d426bc2208fc8d18031e5bce
EidAbdullahi <eidabdullahi10@gmail.com>
2020-12-01 12:19:05 +0300

    commit changes

--- app/request.py
+++ app/request.py
@@ -21,6 +21,7 @@ def get_movies(category):
     with urllib.request.urlopen(get_movies_url) as url:
         get_movies_data = url.read()
         get_movies_response = json.loads(get_movies_data)
+
         movie_results = None
 
         if get_movies_response['results']:
@@ -28,7 +29,6 @@ def get_movies(category):
             movie_results = process_results(movie_results_list)
 
 
-            
     return movie_results
 
 
@@ -36,10 +36,11 @@ def get_movies(category):
 
 def get_movie(id):
     get_movie_details_url = base_url.format(id,api_key)
+
     with urllib.request.urlopen(get_movie_details_url) as url:
         movie_details_data = url.read()
         movie_details_response = json.loads(movie_details_data)
-        
+
         movie_object = None
         if movie_details_response:
             id = movie_details_response.get('id')
@@ -54,14 +55,11 @@ def get_movie(id):
     return movie_object
 
 
-
 def process_results(movie_list):
     '''
     Function  that processes the movie result and transform them to a list of Objects
-
     Args:
         movie_list: A list of dictionaries that contain movie details
-
     Returns :
         movie_results: A list of movie objects
     '''
@@ -77,5 +75,20 @@ def process_results(movie_list):
         if poster:
             movie_object = Movie(id,title,overview,poster,vote_average,vote_count)
             movie_results.append(movie_object)
-            
-    return movie_results
+
+    return movie_results 
+
+def search_movie(movie_name):
+    search_movie_url = 'https://api.themoviedb.org/3/search/movie?api_key={}&query={}'.format(api_key,movie_name)
+    with urllib.request.urlopen(search_movie_url) as url:
+        search_movie_data = url.read()
+        search_movie_response = json.loads(search_movie_data)
+
+        search_movie_results = None
+
+        if search_movie_response['results']:
+            search_movie_list = search_movie_response['results']
+            search_movie_results = process_results(search_movie_list)
+
+
+    return search_movie_results
